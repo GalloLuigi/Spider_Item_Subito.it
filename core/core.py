0396767e171134c58bdcfe1fd2d0e3961514f20b
GalloLuigi <71981111+GalloLuigi@users.noreply.github.com>
2023-11-05 23:56:34 +0100

    remove useless file

--- core/core.py
+++
@@ -1,8 +0,0 @@
-from basic_core import *
-
-
-def find_cars(region, brand, model):
-    link = make_link(region, brand, model)
-    save_link(link)
-    links = make_list_link()
-    open_links(links)
